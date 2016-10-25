4beee5f454dcaa21d9d5bda68e564276afcd3d6b
Alexander-Lysenko <linjay@ya.ru>
2016-10-25 22:43:20 +0300

    Сделал удаление, добавил отображение постера, работаю над поиском и показом подробностей

---
+++ MediaLibrarian/PictureViewer.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MediaLibrarian
+{
+    partial class PictureViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImageBox.Location = new System.Drawing.Point(0, 0);
+            this.ImageBox.MaximumSize = new System.Drawing.Size(800, 768);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(344, 441);
+            this.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ImageBox.TabIndex = 0;
+            this.ImageBox.TabStop = false;
+            // 
+            // PictureViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(344, 441);
+            this.Controls.Add(this.ImageBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximumSize = new System.Drawing.Size(720, 720);
+            this.Name = "PictureViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PictureViewer";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Deactivate += new System.EventHandler(this.PictureViewer_Deactivate);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PictureViewer_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox ImageBox;
+
+    }
+}
---
+++ MediaLibrarian/PictureViewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MediaLibrarian
+{
+    public partial class PictureViewer : Form
+    {
+        public PictureViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void PictureViewer_Deactivate(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PictureViewer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) Close();
+        }
+    }
+}
