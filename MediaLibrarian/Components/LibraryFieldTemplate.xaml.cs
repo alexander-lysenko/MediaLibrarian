cbda235a8f5e5f45e02482bde4c56e30f1a5b3da
Alexander Lysenko <linjay@ya.ru>
2023-10-12 00:56:39 +0300

    Fix LibraryItem component

---
+++ MediaLibrarian/Components/LibraryFieldTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MediaLibrarian.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для LibraryFieldTemplate.xaml
+    /// </summary>
+    public partial class LibraryFieldTemplate : UserControl
+    {
+        public LibraryFieldTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
